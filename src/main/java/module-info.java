1d5632c1e250d55a3d57ad4fc02695825de88cef
George <george.chirikov@metropolia.fi>
2024-12-10 02:13:07 +0200

    refactor(controller): moved to separate package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module org.example.astronomicalcalculationsapp {
 
     opens org.example.astronomicalcalculationsapp to javafx.fxml;
     exports org.example.astronomicalcalculationsapp;
+    exports org.example.astronomicalcalculationsapp.controller;
+    opens org.example.astronomicalcalculationsapp.controller to javafx.fxml;
 }
--- src/main/java/org/example/astronomicalcalculationsapp/controller/MainController.java
+++ src/main/java/org/example/astronomicalcalculationsapp/controller/MainController.java
@@ -1,4 +1,4 @@
-package org.example.astronomicalcalculationsapp;
+package org.example.astronomicalcalculationsapp.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
