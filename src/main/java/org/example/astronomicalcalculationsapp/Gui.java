843b0c611b77e96b2b89899ca3536fddb50cd724
George <george.chirikov@metropolia.fi>
2024-12-16 14:12:07 +0200

    add(style): css file to project

--- src/main/java/org/example/astronomicalcalculationsapp/Gui.java
+++ src/main/java/org/example/astronomicalcalculationsapp/Gui.java
@@ -12,6 +12,7 @@ public class Gui extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 640);
+        scene.getStylesheets().add(getClass().getResource("/org/example/astronomicalcalculationsapp/styles.css").toExternalForm());
         stage.setTitle("Astronomical Calculations App");
         stage.setScene(scene);
         stage.show();
